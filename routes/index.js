4f61ddcfed92fe1235f0dd811024c917766fdee2
Anton Lehrberg <lehrberg.anton@gmail.com>
2024-05-20 20:52:02 +0200

    Modify route.

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,6 @@ const express = require("express");
 const controllers = require("../controllers");
 const router = express.Router();
 
-// router
-//   .route("/member")
-//   .get(middleware.authorize, controllers.getAllMembers)
-//   .post(middleware.authorize, controllers.createMember);
-// router
-//   .route("/member/:personnummer")
-//   .get(controllers.getMember)
-//   .put(middleware.authorize, controllers.updateMember)
-//   .delete(middleware.authorize, controllers.deleteMember);
-
 router.route("/products/categories").get(controllers.getProductsCategories);
 
 router.route("/products/:sex").get(controllers.getProductsSex);
@@ -19,7 +9,7 @@ router
   .route("/products/:sex/:categories")
   .get(controllers.getProductsSexCategories);
 
-router.route("/products/best-sellers/:amount").get(controllers.getBestSellers);
+router.route("/best-sellers/:amount").get(controllers.getBestSellers);
 
 router.route("/product/color/:id").get(controllers.getProductColor);
 router.route("/product/:id/colors").get(controllers.getProductColors);
